761b2c6f2464a824ca98c01b6ad13f8e9118450e
Nicos <hfnovember@hotmail.com>
2018-09-15 16:13:53 +0300

    Quiz

--- docs/js/APIUtils.js
+++ docs/js/APIUtils.js
@@ -4,3 +4,4 @@ const API_STATISTICS = API_BASE_URL + "date-statistics";
 const API_PERCENTAGES = API_BASE_URL + "percentages";
 const API_TIMESERIES = API_BASE_URL + "timeseries";
 const API_MEASUREUNITS = API_BASE_URL + "measure-units";
+const API_QUIZ = API_BASE_URL + "quiz";
---
+++ docs/js/QuizClasses.js
@@ -0,0 +1,95 @@
+class Question {
+
+    get question() {
+        return this._question;
+    }
+
+    get answers() {
+        return this._answers;
+    }
+    constructor(question, answers) {
+
+        this._question = question;
+        this._answers = answers;
+    }
+
+}
+
+class Answer {
+
+    get answer() {
+        return this._answer;
+    }
+
+    get correct() {
+        return this._correct;
+    }
+
+    constructor(answer, correct) {
+        this._answer = answer;
+        this._correct = correct;
+    }
+
+}
+
+class Quiz {
+
+    constructor(questions) {
+        this._questions = questions;
+        this._currentQuestionIndex = 0;
+    }
+
+    get currentQuestionIndex() {
+        return this._currentQuestionIndex;
+    }
+
+    get questions() {
+        return this._questions;
+    }
+
+    /**
+     * Answers a question.
+     */
+    answerQuestion(answer) {
+        //If all questions have been answered:
+        if (this._currentQuestionIndex >= questions.length) {
+            return -1;
+        }
+        //If there are questions to be answered:
+        else {
+
+            //Get the next question:
+            var currentQuestion = questions[this._currentQuestionIndex];
+
+            //Find the correct answer:
+            var correctAnswer;
+            for (var i = 0; i < currentQuestion.answers.length; i++) {
+                if (currentQuestion.answers[i].correct) {
+                    correctAnswer = currentQuestion.answers[i];
+                    break;
+                }
+            }
+
+            //Compare the user's answer with the correct answer:
+            var result;
+            if (correctAnswer.answer === answer) {
+                result = true;
+            }
+            result = false;
+            return result;
+        }
+    }
+
+    /**
+     * Gets the contents of the next question, or false if quiz is over.
+     */
+    getNextQuestion() {
+        if (this._currentQuestionIndex >= this._questions.length) {
+            return false;
+        }
+        else {
+            return this._questions[this._currentQuestionIndex];
+        }
+    }
+
+}
