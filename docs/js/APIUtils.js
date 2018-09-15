9602e2bd04f066eec0c8df613496e9c66875fb07
Nicos <hfnovember@hotmail.com>
2018-09-15 11:02:14 +0300

    Added page: SelectMeasureUnit and its AJAX

---
+++ docs/js/APIUtils.js
@@ -0,0 +1,6 @@
+const API_BASE_URL = "https://cyprus-water.appspot.com/api/";
+const API_DAMS  = API_BASE_URL + "dams";
+const API_STATISTICS = API_BASE_URL + "date-statistics";
+const API_PERCENTAGES = API_BASE_URL + "percentages";
+const API_TIMESERIES = API_BASE_URL + "timeseries";
+const API_MEASUREUNITS = API_BASE_URL + "";  //TODO complete url
